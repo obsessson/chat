8cd2de2f6f36bfe66a7f580cd4f6b00830181902
obsessson <khomenkomargoo@gmail.com>
2026-04-18 19:32:55 +0300

    4

--- static/script.js
+++ static/script.js
@@ -1,1 +1,9 @@
-alert('Hello!')
+const socket = io();
+
+form.addEventListener('submit', function(e) {
+  e.preventDefault();
+  if (input.value) {
+    socket.emit('new_message', input.value);
+    input.value = '';
+  }
+});
