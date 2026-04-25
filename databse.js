38e136b846793660dbd9a6649e21d3ea6431b0ca
obsessson <khomenkomargoo@gmail.com>
2026-04-25 19:11:07 +0300

    6

--- databse.js
+++ databse.js
@@ -5,3 +5,27 @@ const exists = fs.existsSync(dbFile);
 const sqlite3 = require("sqlite3").verbose();
 const dbWrapper = require("sqlite");
 let db;
+
+dbWrapper
+.open({
+    filename: dbFile,
+    driver: sqlite3.Database
+})
+.then(async dBase => {
+    db = dBase
+    try {
+        if (!exists) {
+            await db.run(
+                `CREATE TABLE user(
+                user_id INTEGER PRIMARY KEY AUTOINCREMENT,
+                login TEXT,
+                password TEXT
+                )`
+            )
+        } else {
+            console.log(await db.all("SELECT * from user"))
+        }
+    } catch (dbError) {
+        console.error(dbError)
+    }
+})
